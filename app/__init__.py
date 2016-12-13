59c480fb3981d686dae334e2d0803a1180dbf576
ben hong <benbosukhong@gmail.com>
2016-12-12 23:19:18 -0600

    bug fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_login import LoginManager
 from config import WEB_CLIENT_ID, MODE
 
-if MODE != 'local':
+app = Flask(__name__)
+if MODE != 'deploy':
     from flask_sslify import SSLify
     sslify = SSLify(app)
-app = Flask(__name__)
 app.config.from_object('config')
 login_manager = LoginManager()
 login_manager.init_app(app)
