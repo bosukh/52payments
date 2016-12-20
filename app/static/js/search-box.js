7a69a7565e90464f19e463d6c77068d41d0e4c1d
ben hong <benbosukhong@gmail.com>
2016-12-20 00:12:47 -0600

    in the middle of css/html facelift

--- app/static/js/search-box.js
+++ app/static/js/search-box.js
@@ -47,9 +47,9 @@ var select_items_title = function(items_title){
   items_title = items_title.parentElement;
   var items_titles = document.getElementsByClassName('items-title-div');
   for (var i = 0; i < items_titles.length; i++){
-    items_titles[i].className = 'items-title-div';
+    items_titles[i].className = 'items-title-div column';
   };
-  items_title.className = 'selected items-title-div'
+  items_title.className = 'selected items-title-div column'
   var current_num = items.childNodes.length
   for (var i = 1; i < current_num; i++){
     items.childNodes[1].remove();
