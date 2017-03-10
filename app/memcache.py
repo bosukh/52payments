5b9a2f2c0afe38adac62232dcbdbc1c66903a203
ben hong <benbosukhong@gmail.com>
2017-03-09 23:22:24 -0600

    variable name change, and comments

--- app/memcache.py
+++ app/memcache.py
@@ -1,22 +1,30 @@
 from google.appengine.api import memcache
 
-def mc_upsert(key, data):
-    if not key or not data:
+def mc_upsert(key, value):
+    '''
+    Given a key/value pair, update or insert into memcache
+    '''
+    if not key or not value:
         return False
     dat = memcache.get(key)
     if dat:
-        memcache.replace(key, data)
+        memcache.replace(key, value)
     else:
-        memcache.add(key, data)
+        memcache.add(key, value)
     return True
 
 def mc_getsert(key, func):
+    '''
+    Given key/func pair,
+    If value is there for the key, return the value
+    else, run func(usually a query) and insert to memcache and return the value
+    '''
     if not key:
         return False
     dat = memcache.get(key)
     if dat:
         return dat
     else:
-        data = func()
-        memcache.add(key, data)
-        return data
+        value = func()
+        memcache.add(key, value)
+        return value
