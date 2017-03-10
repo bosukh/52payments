d789a4e2630bc3f11bb02ffe75697072e3be0216
ben hong <benbosukhong@gmail.com>
2017-03-09 23:20:43 -0600

    just changing dict accessing []->get.

--- app/forms.py
+++ app/forms.py
@@ -12,14 +12,13 @@ from flask import request, url_for, redirect
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-           ref_url.netloc == test_url.netloc
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
 
 def get_redirect_target():
     logging.debug(request.args.get('next'))
     logging.debug(request.referrer)
-    logging.debug(session['initial_referrer'])
-    for target in request.args.get('next'), session['initial_referrer'], request.referrer:
+    logging.debug(session.get('initial_referrer'))
+    for target in request.args.get('next'), session.get('initial_referrer'), request.referrer:
         if not target:
             continue
         if is_safe_url(target):
