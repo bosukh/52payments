918e49987b735125d373c96f7b9e255ed80b2ba6
ben hong <benbosukhong@gmail.com>
2017-03-10 22:11:57 -0600

    delete previous models.py

--- app/modelss.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from google.appengine.ext import ndb
-from datetime import datetime
-from functools import wraps
-from .sticky_notes import add_notes
-from flask_login import current_user
-from uuid import uuid1
