a27ecee6a4c203561a7882509a1cb02749390b0b
ben hong <benbosukhong@gmail.com>
2017-03-12 14:45:03 -0500

    move redirect related fxns in one place.

--- app/redirect_check.py
+++ app/redirect_check.py
@@ -1,4 +1,7 @@
+import logging
 from config import ALLOWED_ORIGINS, LOGIN_REQUIRED_LIST
+from urlparse import urlparse, urljoin
+from flask import request, session
 
 def check_referrer_auth_requirement(referrer, avoid_list=[]):
     referrer = str(referrer)
@@ -15,3 +18,17 @@ def check_referrer_origin(referrer):
         if referrer[:len(url)] == url:
             return True
     return False
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+def get_redirect_target():
+    logging.debug(request.referrer)
+    logging.debug(session.get('initial_referrer'))
+    for target in request.args.get('next'), session.get('initial_referrer'), request.referrer:
+        if not target:
+            continue
+        if is_safe_url(target):
+            return target
